c8725a3e2e5ab1abcb6573e6c3a501ab2a576020
Thomas Ruggeri <truggeri@gmail.com>
2020-06-14 15:37:52 -0400

    Further organization updates

---
+++ app/javascript/components/GameBoard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="columns is-centered">
+    <div class="column is-two-thirds">
+      <div v-for="row in game_size" v-bind:key="row" class="columns">
+        <div v-for="col in game_size" v-bind:key="col" class="column is-one-fifth is-vcentered">
+          <Card v-bind:color="cardColor(row, col)" v-bind:id="cardId(row, col)" word="Foo" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "./Card"
+
+export default {
+  name: "GameBoard",
+  components: {
+    Card
+  },
+  data: function () {
+    return {
+      game_size: 5
+    }
+  },
+  methods: {
+    cardColor(row, column) {
+      const seed = row * 2 + column;
+      return seed % 2 == 0 ? "blue" : "red";
+    },
+    cardId(row, column) {
+      return `card_${row}_${column}`
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,44 +1,22 @@
 <template>
-  <div id="app" class="columns is-centered">
-    <div class="column is-two-thirds">
-      <div v-for="row in game_size" v-bind:key="row" class="columns">
-        <div v-for="col in game_size" v-bind:key="col" class="column is-one-fifth is-vcentered">
-          <Card v-bind:color="cardColor(row, col)" v-bind:id="cardId(row, col)" word="Foo" />
-        </div>
-      </div>
-    </div>
+  <div id="app">
+    <GameBoard />
   </div>
 </template>
 
 <script>
-import Card from "./Card"
+import GameBoard from "./GameBoard"
 
 export default {
   components: {
-    Card
+    GameBoard
   },
   data: function () {
-    return {
-      game_size: 5
-    }
-  },
-  methods: {
-    cardColor(row, column) {
-      const seed = row * 2 + column;
-      return seed % 2 == 0 ? "blue" : "red";
-    },
-    cardId(row, column) {
-      return `card_${row}_${column}`
-    }
+    return {}
   }
 }
 </script>
 
 <style lang="scss">
 @import "packs/stylesheets/application.scss";
-
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
