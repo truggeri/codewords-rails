bac91effa0a1e142953924ef5b30d0ca251e61a0
Thomas Ruggeri <truggeri@gmail.com>
2020-06-14 17:44:30 -0400

    Adding a footer

--- app/javascript/packs/game.js
+++ app/javascript/packs/game.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     render: h => h(App)
   }).$mount()
-  document.body.appendChild(app.$el)
+  document.getElementById("app").appendChild(app.$el)
 
   console.log(app)
 })
