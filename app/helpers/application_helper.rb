6784f26e5755c62968b9e51bcf63847766aeaa0d
Thomas Ruggeri <truggeri@gmail.com>
2020-06-15 10:42:18 -0400

    Update to flash handler

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def flash_notice(flash)
+    capture do
+      flash.each do |message_type, message| 
+        if %w[error alert].include?(message_type)
+          message_icon = "⚠️"
+          message_type = "danger"
+        end 
+        message_type = "success" if %w[notice].include?(message_type)
+
+        msg = safe_join([message_icon, message], " ")
+        concat(content_tag(:div, msg, class: "notification is-#{message_type}"))
+      end 
+    end
+  end
 end
